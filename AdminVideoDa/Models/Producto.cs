92b6ea94f28f020ca5d09b39d31746e68e10eaa2
Fernando Diaz <dfer4738@gmail.com>
2023-11-20 20:11:43 +0100

    mejora en busqueda

--- AdminVideoDa/Models/Producto.cs
+++ AdminVideoDa/Models/Producto.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public int IdCategoria { get; set; }
+        public Categoria Categoria { get; set; }
         public int IdConsola { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
